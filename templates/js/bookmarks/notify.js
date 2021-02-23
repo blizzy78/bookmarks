0407db04fa54de1ea9f7c8a7ca7fa6e362fa432c
Maik Schreiber <blizzy@blizzy.de>
2021-02-23 18:34:16 +0000

    use html() to set inner HTML

--- templates/js/bookmarks/notify.js
+++ templates/js/bookmarks/notify.js
@@ -3,15 +3,15 @@ define(() => {
 		'message': textHtml => {
 			let id = 'toast-' + Date.now();
 			let html = '<div id="' + id + '" class="toast bg-success text-light" role="alert" aria-live="assertive" aria-atomic="true">' +
-				'<div class="toast-body">' +
-				textHtml +
-				'</div>' +
+				'<div class="toast-body"></div>' +
 				'</div>';
 			$('.toast-container').append($.parseHTML(html));
 			let el = $('#' + id);
-			el.on('hidden.bs.toast', () => {
-				el.remove();
-			});
+			el
+				.on('hidden.bs.toast', () => {
+					el.remove();
+				})
+				.find('.toast-body').html(textHtml);
 			new bootstrap.Toast(el[0]).show();
 		}
 	};
