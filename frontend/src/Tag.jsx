991d70719db954f95d7cb13f2092193958daaf0d
Maik Schreiber <blizzy@blizzy.de>
2021-04-20 21:09:48 +0200

    improve styles

--- frontend/src/Tag.jsx
+++ frontend/src/Tag.jsx
@@ -7,8 +7,8 @@ export default class Tag extends React.Component {
     let cssClass = classNames(
       'px-2 py-1 rounded ' +
       'bg-green-500 text-white ' +
-      'font-normal outline-none focus:outline-none ' +
-      'dark:bg-green-700 dark:text-green-50',
+      'font-normal outline-none focus:outline-none hover:bg-green-600 ' +
+      'dark:bg-green-700 dark:text-green-100 dark:hover:bg-green-600 dark:hover:text-green-50',
       this.props.className)
     return <button className={cssClass} type="button" onClick={this.props.onClick}>{this.props.tag}</button>
   }
