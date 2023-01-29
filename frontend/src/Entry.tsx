89db15d07328febfc2460e7d313c2a386bedfe95
Maik Schreiber <blizzy@blizzy.de>
2023-01-29 05:12:36 +0100

    cosmetic changes

--- frontend/src/Entry.tsx
+++ frontend/src/Entry.tsx
@@ -10,13 +10,17 @@ export const Entry = ({ hit, onEditClick }: {
   }) => (
 
   <div className="flex flex-col gap-2 items-start">
-    <a className="text-sm dark:text-slate-400 dark:hover:text-slate-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-300 w-full truncate" href={hit.url} rel="noreferrer noopener" dangerouslySetInnerHTML={{__html: hit.urlHTML}}/>
+    <a className="text-sm dark:text-slate-400 dark:hover:text-slate-300 hover:underline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-300 w-full truncate"
+      href={hit.url} rel="noreferrer noopener" dangerouslySetInnerHTML={{__html: hit.urlHTML}}/>
 
     <div className="flex flex-col gap-1">
       <h3 className="text-lg font-semibold flex flex-row gap-3 items-center">
-        <a className="dark:hover:text-slate-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-300" href={hit.url} rel="noreferrer noopener" dangerouslySetInnerHTML={{__html: hit.titleHTML}}/>
+        <a className="dark:hover:text-slate-50 hover:underline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-300"
+          href={hit.url} rel="noreferrer noopener" dangerouslySetInnerHTML={{__html: hit.titleHTML}}/>
 
-        <Mantine.ActionIcon title="Edit" onClick={onEditClick} className="active:!translate-y-0 !font-inherit !text-lg dark:hover:bg-slate-600 dark:text-slate-500 dark:hover:text-slate-50 dark:focus:!outline-indigo-300">
+        <Mantine.ActionIcon className="active:!translate-y-0 !font-inherit !text-lg dark:hover:bg-slate-600 dark:text-slate-500 dark:hover:text-slate-50 dark:focus:!outline-indigo-300"
+          title="Edit" onClick={onEditClick}>
+
           <FontAwesome.FontAwesomeIcon icon={FontAwesomeSolid.faEdit}/>
         </Mantine.ActionIcon>
       </h3>
