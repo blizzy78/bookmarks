118113649cf58a4992da2f5b2c63dec8589d9b58
Maik Schreiber <blizzy@blizzy.de>
2023-10-28 01:24:47 +0200

    update dependencies

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -5,12 +5,12 @@ import { useCallback } from 'react'
 import { z } from 'zod'
 
 const hitSchema = z.object({
-  id: z.string().nonempty(),
-  url: z.string().nonempty(),
-  urlHTML: z.string().nonempty(),
-  titleHTML: z.string().nonempty(),
+  id: z.string().min(1),
+  url: z.string().min(1),
+  urlHTML: z.string().min(1),
+  titleHTML: z.string().min(1),
   descriptionHTML: z.string(),
-  tags: z.array(z.string().nonempty()).optional(),
+  tags: z.array(z.string().min(1)).min(1).optional(),
 })
 
 export type Hit = z.infer<typeof hitSchema>
@@ -23,11 +23,11 @@ const resultSchema = z.object({
 export type Result = z.infer<typeof resultSchema>
 
 const bookmarkSchema = z.object({
-  objectID: z.string().nonempty().optional(),
-  url: z.string().nonempty(),
-  title: z.string().nonempty(),
+  objectID: z.string().min(1).optional(),
+  url: z.string().min(1),
+  title: z.string().min(1),
   description: z.string(),
-  tags: z.array(z.string().nonempty()).optional(),
+  tags: z.array(z.string().min(1)).min(1).optional(),
 })
 
 export type Bookmark = z.infer<typeof bookmarkSchema>
@@ -50,11 +50,11 @@ export type DeleteBookmarkData = {
   onDeleted(): void
 }
 
-const tagCountsSchema = z.record(z.string().nonempty(), z.number().min(1))
+const tagCountsSchema = z.record(z.string().min(1), z.number().min(1))
 
 export type TagCounts = z.infer<typeof tagCountsSchema>
 
-const tagsListSchema = z.array(z.string().nonempty())
+const tagsListSchema = z.array(z.string().min(1))
 
 export type TagsList = z.infer<typeof tagsListSchema>
 
@@ -64,53 +64,55 @@ export const createQueryClient = () =>
       queries: {
         refetchOnWindowFocus: false,
         staleTime: Infinity,
-        cacheTime: Infinity,
+        gcTime: Infinity,
         retry: false,
       },
     },
   })
 
 export const useSearch = (query: string) =>
-  ReactQuery.useQuery(
-    ['search', query],
-    async () =>
-      resultSchema.parse(await ky.get(`/rest/bookmarks?q=${encodeURIComponent(query)}`, { parseJson }).json()),
-    {
-      enabled: query.trim() !== '',
-      staleTime: 15 * 60 * 1000,
-    }
-  )
+  ReactQuery.useQuery({
+    queryKey: ['search', query],
+
+    async queryFn() {
+      return resultSchema.parse(await ky.get(`/rest/bookmarks?q=${encodeURIComponent(query)}`, { parseJson }).json())
+    },
+
+    enabled: query.trim() !== '',
+    staleTime: 15 * 60 * 1000,
+  })
 
 const useInvalidateSearch = () => {
   const queryClient = ReactQuery.useQueryClient()
 
   return useCallback(() => {
-    queryClient.cancelQueries(['search'])
-    queryClient.invalidateQueries(['search'])
+    queryClient.cancelQueries({ queryKey: ['search'] })
+    queryClient.invalidateQueries({ queryKey: ['search'] })
   }, [queryClient])
 }
 
 export const useBookmark = (objectID?: string) =>
-  ReactQuery.useQuery(
-    !!objectID ? ['bookmark', objectID] : ['bookmark.new'],
-    async () =>
-      !!objectID
+  ReactQuery.useQuery({
+    queryKey: !!objectID ? ['bookmark', objectID] : ['bookmark.new'],
+
+    async queryFn() {
+      return !!objectID
         ? bookmarkSchema.parse(await ky.get(`/rest/bookmark/${encodeURIComponent(objectID)}`, { parseJson }).json())
-        : emptyBookmark,
-    {
-      staleTime: 15 * 60 * 1000,
-    }
-  )
+        : emptyBookmark
+    },
+
+    staleTime: 15 * 60 * 1000,
+  })
 
 const useInvalidateBookmark = () => {
   const queryClient = ReactQuery.useQueryClient()
 
   return useCallback(
     (objectID: string) => {
-      queryClient.cancelQueries(['bookmark', objectID])
-      queryClient.invalidateQueries(['bookmark', objectID])
+      queryClient.cancelQueries({ queryKey: ['bookmark', objectID] })
+      queryClient.invalidateQueries({ queryKey: ['bookmark', objectID] })
     },
-    [queryClient]
+    [queryClient],
   )
 }
 
@@ -119,19 +121,22 @@ export const useCreateBookmark = () => {
   const invalidateAllTags = useInvalidateAllTags()
   const invalidateAllTagCounts = useInvalidateAllTagCounts()
 
-  const mutation = ReactQuery.useMutation<unknown, Error, SaveBookmarkData>(
-    (data) => ky.post('/rest/bookmark', { json: data.bookmark, timeout: 30000 }),
-    {
-      onMutate: (variables) => variables.onCreating(),
+  const mutation = ReactQuery.useMutation({
+    mutationFn(data: SaveBookmarkData) {
+      return ky.post('/rest/bookmark', { json: data.bookmark, timeout: 30000 })
+    },
 
-      onSuccess: (_, variables) => {
-        variables.onCreated()
-        invalidateSearch()
-        invalidateAllTags()
-        invalidateAllTagCounts()
-      },
-    }
-  )
+    onMutate(variables: SaveBookmarkData) {
+      variables.onCreating()
+    },
+
+    onSuccess(_, variables: SaveBookmarkData) {
+      variables.onCreated()
+      invalidateSearch()
+      invalidateAllTags()
+      invalidateAllTagCounts()
+    },
+  })
 
   return mutation.mutateAsync
 }
@@ -142,24 +147,26 @@ export const useUpdateBookmark = () => {
   const invalidateAllTags = useInvalidateAllTags()
   const invalidateAllTagCounts = useInvalidateAllTagCounts()
 
-  const mutation = ReactQuery.useMutation<unknown, Error, SaveBookmarkData>(
-    (data) =>
-      ky.put(`/rest/bookmark/${encodeURIComponent(data.bookmark.objectID as string)}`, {
+  const mutation = ReactQuery.useMutation({
+    mutationFn(data: SaveBookmarkData) {
+      return ky.put(`/rest/bookmark/${encodeURIComponent(data.bookmark.objectID as string)}`, {
         json: data.bookmark,
         timeout: 30000,
-      }),
-    {
-      onMutate: (variables) => variables.onCreating(),
+      })
+    },
 
-      onSuccess: (_, variables) => {
-        variables.onCreated()
-        invalidateBookmark(variables.bookmark.objectID as string)
-        invalidateSearch()
-        invalidateAllTags()
-        invalidateAllTagCounts()
-      },
-    }
-  )
+    onMutate(variables: SaveBookmarkData) {
+      variables.onCreating()
+    },
+
+    onSuccess(_, variables: SaveBookmarkData) {
+      variables.onCreated()
+      invalidateBookmark(variables.bookmark.objectID as string)
+      invalidateSearch()
+      invalidateAllTags()
+      invalidateAllTagCounts()
+    },
+  })
 
   return mutation.mutateAsync
 }
@@ -170,56 +177,63 @@ export const useDeleteBookmark = () => {
   const invalidateAllTags = useInvalidateAllTags()
   const invalidateAllTagCounts = useInvalidateAllTagCounts()
 
-  const mutation = ReactQuery.useMutation<unknown, Error, DeleteBookmarkData>(
-    (data) => ky.delete(`/rest/bookmark/${encodeURIComponent(data.objectID)}`, { timeout: 30000 }),
-    {
-      onMutate: (variables) => variables.onDeleting(),
+  const mutation = ReactQuery.useMutation({
+    mutationFn(data: DeleteBookmarkData) {
+      return ky.delete(`/rest/bookmark/${encodeURIComponent(data.objectID)}`, { timeout: 30000 })
+    },
 
-      onSuccess: (_, variables) => {
-        variables.onDeleted()
-        invalidateBookmark(variables.objectID)
-        invalidateSearch()
-        invalidateAllTags()
-        invalidateAllTagCounts()
-      },
-    }
-  )
+    onMutate(variables: DeleteBookmarkData) {
+      variables.onDeleting()
+    },
+
+    onSuccess(_, variables: DeleteBookmarkData) {
+      variables.onDeleted()
+      invalidateBookmark(variables.objectID)
+      invalidateSearch()
+      invalidateAllTags()
+      invalidateAllTagCounts()
+    },
+  })
 
   return mutation.mutateAsync
 }
 
 export const useAllTags = () =>
-  ReactQuery.useQuery(
-    ['bookmarks.tags'],
-    async () => tagsListSchema.parse(await ky.get('/rest/bookmarks/tags', { parseJson }).json()),
-    {
-      staleTime: 15 * 60 * 1000,
-    }
-  )
+  ReactQuery.useQuery({
+    queryKey: ['bookmarks.tags'],
+
+    async queryFn() {
+      return tagsListSchema.parse(await ky.get('/rest/bookmarks/tags', { parseJson }).json())
+    },
+
+    staleTime: 15 * 60 * 1000,
+  })
 
 const useInvalidateAllTags = () => {
   const queryClient = ReactQuery.useQueryClient()
 
   return useCallback(() => {
-    queryClient.cancelQueries(['bookmarks.tags'])
-    queryClient.invalidateQueries(['bookmarks.tags'])
+    queryClient.cancelQueries({ queryKey: ['bookmarks.tags'] })
+    queryClient.invalidateQueries({ queryKey: ['bookmarks.tags'] })
   }, [queryClient])
 }
 
 export const useAllTagCounts = () =>
-  ReactQuery.useQuery(
-    ['bookmarks.tagCounts'],
-    async () => tagCountsSchema.parse(await ky.get('/rest/bookmarks/tagCounts', { parseJson }).json()),
-    {
-      staleTime: 15 * 60 * 1000,
-    }
-  )
+  ReactQuery.useQuery({
+    queryKey: ['bookmarks.tagCounts'],
+
+    async queryFn() {
+      return tagCountsSchema.parse(await ky.get('/rest/bookmarks/tagCounts', { parseJson }).json())
+    },
+
+    staleTime: 15 * 60 * 1000,
+  })
 
 const useInvalidateAllTagCounts = () => {
   const queryClient = ReactQuery.useQueryClient()
 
   return useCallback(() => {
-    queryClient.cancelQueries(['bookmarks.tagCounts'])
-    queryClient.invalidateQueries(['bookmarks.tagCounts'])
+    queryClient.cancelQueries({ queryKey: ['bookmarks.tagCounts'] })
+    queryClient.invalidateQueries({ queryKey: ['bookmarks.tagCounts'] })
   }, [queryClient])
 }
